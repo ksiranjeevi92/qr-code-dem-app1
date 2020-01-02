e103a332710da9d2d2ece05c59b37013405041dd
ksiranjeevi92 <rksj666@gmail.com>
2020-01-02 19:29:49 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component,ViewChild,ElementRef, AfterViewInit, OnInit } from '@angular/core';
 import {PrinterService} from './printer.service';
 
+let barcodeId = null;
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -13,6 +15,11 @@ export class AppComponent implements AfterViewInit,OnInit {
   @ViewChild('des')des: ElementRef;
   @ViewChild('uom')uom: ElementRef;
   @ViewChild('qty')qty: ElementRef;
+  @ViewChild('grower')grower: ElementRef;
+  @ViewChild('timeStampCode')timeStampCode: ElementRef;
+  @ViewChild('bid')bid: ElementRef;
+  
+  
 
   constructor(private printerService: PrinterService) {}
 
@@ -33,6 +40,7 @@ ngOnInit() {
   }
 
  print(pdata) {
+      let bId = barcodeId;
    let printContent = ``;
 
       printContent = 
@@ -139,6 +147,113 @@ ngOnInit() {
       </body>
       </html>
       `
+
+
+      printContent = `
+        <html>
+  
+        <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+          <style>
+          html, body {
+            padding: 0;
+            margin: 0;
+          }
+              @media print{
+              .pagebreak {
+                clear: both;
+                page-break-after: always;
+            }
+                body {
+                  margin-top:0 !important;
+                  margin-right: 1mm !important;
+                  margin-left: 1mm !important;
+                  margin-bottom: 0;
+                  padding: 0;
+                  }
+              @page {
+                margin: 0;
+                }
+              }
+            .page-break {
+                page-break-after:always;
+              }
+            
+              .container {
+                margin-top: 1mm;
+                margin-bottom: 1mm;
+                margin-left: 0 !important;
+                padding: 0;
+                background: blck;
+                position: relative;
+              }
+          </style>
+          <script>
+          
+          </script>
+        </head>
+  
+        <body>
+        <div>
+        ${this.printData.map((item, i) => ` 
+              <div class="container" style="height: 22mm;width: 47mm; float: ${(i + 1) % 2 !== 0 ? 'left' : 'right'}">
+  
+  
+                <div id="label1" style="display:${(i + 1) % 2 !== 0 ? '' : 'none'};margin-bottom: 5mm;margin-right: 1mm;margin-left: 4mm;margin-top: 0;
+                height: 19mm;width: 42mm;background: gren">
+                <span></span>
+                  <div id="table1" style="display: table;height: 19mm;width: 154px;border-spacing: 4px;">
+                    <div style="display: table-row;height: 19mm;width: 150px;">
+                      <div style="display: table-cell;width: 70px;max-width: 70px;vertical-align: middle;">
+                        <div>
+                          <img src="${"https://chart.googleapis.com/chart?cht=qr&chl=" + (pdata.itemCode + '%09' + pdata.growerCode + '%09' + pdata.packUoMCode + '%09' + pdata.timeStampCode + '%09' + '0' + '%13') + "&chs=67x62&chld=L|0"}">
+                        </div>
+                        <div style="width: 70px;max-width: 70px;overflow: hidden;font-family: Impact;font-size: 12px;text-align: center">${pdata.itemCode}</div>
+                      </div>
+                        
+                        <div style="display: table-cell;width: 80px;max-width: 80px;height: 19mm;position: relative">
+                          <div style="display: inline-block;font-family: Impact;font-size:10px;width: 80px;max-width: 80px;overflow: hidden;height: 17mm;max-height: 17mm;position: relative">                         
+                              <div>${pdata.displayName}</div>
+                          </div>
+                          <div style="font-family: Calibri;font-size: 12px;font-weight: bold;height: 3mm;max-height: 3mm;display: flex;align-item: center">
+                            <span>${pdata.timeStampCode}</span>
+                          </div>
+                          <strong style="text-align: end;bottom: 0;text-align: end;display: block;position:absolute;width: 100%;font-size: 9px;top:75px">${bId ? bId : ''}</strong>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                
+                  <div id="labe2" style="display:${(i + 1) % 2 !== 0 ? 'none' : ''};margin-bottom: 5mm;margin-right: 1mm;margin-left: 1mm;margin-top: 0;
+                        height: 19mm;width: 45mm;background: yelow">
+                  <div id="table1" style="display: table;height: 19mm;width: 154px;border-spacing: 4px;">
+                    <div style="display: table-row;height: 19mm;width: 150px;">
+                      <div style="display: table-cell;width: 70px;max-width: 70px;vertical-align: middle;">
+                        <div>
+                          <img src="${"https://chart.googleapis.com/chart?cht=qr&chl=" + (pdata.itemCode + '%09' + pdata.growerCode + '%09' + pdata.packUoMCode + '%09' + pdata.timeStampCode + '%09' + '0' + '%13') + "&chs=67x62&chld=L|0"}">
+                        </div>
+                        <div style="width: 70px;max-width: 70px;overflow: hidden;font-family: Impact;font-size: 12px;text-align: center">${pdata.itemCode}</div>
+                      </div>
+                        
+                        <div style="display: table-cell;width: 80px;max-width: 80px;height: 19mm;position: relative">
+                          <div style="display: inline-block;font-family: Impact;font-size:10px;width: 80px;max-width: 80px;overflow: hidden;height: 17mm;max-height: 17mm;position: relative">                          
+                               <div>${pdata.displayName}</div>
+                          </div>
+                          <div style="font-family: Calibri;font-size: 12px;font-weight: bold;height: 3mm;max-height: 3mm;display: flex;align-item: center">
+                            <span>${pdata.timeStampCode}</span>
+                          </div>
+                          <strong style="text-align: end;bottom: 0;text-align: end;display: block;position:absolute;width: 100%;font-size: 9px;top:75px">${bId ? bId : ''}</strong>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+  
+              </div>
+          `.trim()).join('')}
+        </div>
+        </body>
+        </html>
+        `
 var data = [{
       type: 'html',
       format: 'plain',
@@ -150,26 +265,29 @@ var data = [{
 
  onPrint(data) {
 
-   
+  //  let html = data[0].data;
   
     //  this.printerService.printHTML('Honeywell PC42t (203 dpi) - ESim', data);
+     this.printerService.printHTML('Intermec PD43 (203 dpi)', data);
    
     // this.printerService.printData('Honeywell PC42t (203 dpi) - ESim', data).subscribe(() => {});
     
-   var myWindow = window.open("", "BarCode Print");
-    myWindow.document.write(data[0].data);
+  //  var myWindow = window.open("", "BarCode Print");
+  //   myWindow.document.write(data[0].data);
     
   
-    this.printData = [1];
+  //   this.printData = [1];
 
-    let html = data[0].data;
+  //   let html = data[0].data;
  
 
  }
 
 
  submit() {
-   const comp = {itemCode: this.code.nativeElement.value, description: this.des.nativeElement.value, baseUoMCode: this.uom.nativeElement.value, baseQuantity: this.qty.nativeElement.value};
+   const comp = {itemCode: this.code.nativeElement.value,growerCode: this.grower.nativeElement.value, description: this.des.nativeElement.value,displayName: this.des.nativeElement.value, baseUoMCode: this.uom.nativeElement.value, baseQuantity: this.qty.nativeElement.value, timeStampCode: this.timeStampCode.nativeElement.value};
+   barcodeId = this.bid.nativeElement.value;
+   console.log( this.set.nativeElement.value)
    const loop = this.set.nativeElement.value;
    var arr = [];
    if(loop && loop > 1) {
